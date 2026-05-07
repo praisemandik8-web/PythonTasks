b6dea71f25233037dae67e0887b5c0d8e5742727
Pankyes <praisemandik8@gmail.com>
2026-05-07 09:08:18 +0100

    correct errors

---
+++ chapter4/cube_correct.py
@@ -0,0 +1,7 @@
+def cube(x):
+    """Calculate the cube of x."""
+    return x ** 3
+print('The cube of 2 is', cube(2))
+
+#the error comes from the lack of indentation after declaring the function
+# also the "return" key word is missing, which I have corrected
